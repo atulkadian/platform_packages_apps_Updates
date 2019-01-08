0bb3b44e770809ebc243b4d6c45434cdb4add3eb
AtulKadian <atul2143143@gmail.com>
2019-01-08 06:50:49 +0000

    AICP OTA ;)

--- README.md
+++
@@ -1,4 +0,0 @@
-# System updates app
-### Based on Lineage/CyanogenMod ota app
-
-[Official devices application](https://github.com/Colt-Enigma/colt_official_devices)
--- app/src/main/java/net/cachapa/expandablelayout/ExpandableLayout.java
+++ app/src/main/java/net/cachapa/expandablelayout/ExpandableLayout.java
@@ -20,7 +20,7 @@ import static net.cachapa.expandablelayout.ExpandableLayout.State.COLLAPSING;
 import static net.cachapa.expandablelayout.ExpandableLayout.State.EXPANDED;
 import static net.cachapa.expandablelayout.ExpandableLayout.State.EXPANDING;
 
-import org.colt.ota.R;
+import org.aicp.ota.R;
 
 public class ExpandableLayout extends FrameLayout {
     public interface State {
